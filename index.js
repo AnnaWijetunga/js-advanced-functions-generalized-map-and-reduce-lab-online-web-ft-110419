a183b17652c153f77c824bfc249491c6a089b33b
AnnaWijetunga <annawijetunga@gmail.com>
2020-03-02 14:39:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+function map(sourceArray, callback) {
+    let r = []
+
+    for (let i = 0; i < sourceArray.length; i++) {
+        let theElement = sourceArray[i]
+        r.push(callback(theElement))
+    }
+    return r;
+}
+
+function reduce( sourceArray, callback, starting) {
+    let r = (!!starting) ? starting : sourceArray[0]
+    let i = (!!starting) ? 0:1
+
+    for (; i < sourceArray.length; i++) {
+        r = callback(sourceArray[i], r)
+    }
+
+    return r;
+}
